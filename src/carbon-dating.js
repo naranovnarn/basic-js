3f90bd2240e49cabfad4d68f45cc9f1fa23aea4d
naranovnarn <na.naranov@gmail.com>
2020-02-16 19:44:57 +0300

    fix: solve 1, 2 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if ( (typeof sampleActivity) == "string" && (typeof +sampleActivity) == "number" && isFinite(sampleActivity) && +sampleActivity < 15) {
+  let k = 0.693 / HALF_LIFE_PERIOD
+  let t = (Math.log(MODERN_ACTIVITY / + sampleActivity)) / k
+
+  return +t.toFixed() } 
+  else {
+    return false
+  }
 };
